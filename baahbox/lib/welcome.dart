98a638bf4283bc5985d32001755eea8bd4bdef61
Frédérique Pinson <frederique.pinson@gmail.com>
2023-06-05 13:58:41 +0200

    First Raw version of the Dashboard

--- baahbox/lib/welcome.dart
+++ baahbox/lib/welcome.dart
@@ -11,56 +11,94 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          titleTextStyle: TextStyle(color: Colors.orangeAccent),
+          titleTextStyle: TextStyle(
+              color: Colors.orangeAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 25),
           centerTitle: true,
           title: Text("Baah !"),
+          actions: [
+            Container(
+                width: 30,
+                child: Image.asset(
+                  'assets/images/Dashboard/connexion_main@2x.png',
+                )),
+            Container(
+                width: 30,
+                child: Image.asset(
+                  'assets/images/Dashboard/settings_icon@2x.png',
+                )),
+            Icon(Icons.more_vert),
+          ],
         ),
         backgroundColor: Colors.white,
         body: Container(
           child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // SizedBox(height: 50,),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Color(0xFF70576E))),
-                  child: Container(
-                      height: 100,
-                      width: 300,
-                      child: const Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image(image: AssetImage('assets/images/Dashboard/menu_etoile@2x.png')),
-                        Text(
-                          'Fais briller l\'étoile !',
-                          style: TextStyle(color: Colors.white),
-                          textAlign: TextAlign.end,
-                          textScaleFactor: 1.0,
-                        )
-                      ])),
-                  onPressed: () => Get.to(() => SheepGame())),
-
-              ElevatedButton(
-                  child: Text('Balloon'),
-                  onPressed: () => Get.to(() => WelcomePage())),
-
-              ElevatedButton(
-                  child: Text('Sheep'),
-                  onPressed: () => Get.to(() => WelcomePage())),
-
-              ElevatedButton(
-                  child: Text('SpaceGame'),
-                  onPressed: () => Get.to(() => WelcomePage())),
-
-              ElevatedButton(
-                  child: Text('Toad'),
-                  onPressed: () => Get.to(() => WelcomePage())),
-            ],
-            //wrap
-          ),
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GameRow('assets/images/Dashboard/menu_etoile@2x.png',
+                    0xFF70576E, 'Star', 1),
+                GameRow('assets/images/Dashboard/menu_ballon@2x.png',
+                    0xFFE26F3B, 'Balloon', 1),
+                GameRow('assets/images/Dashboard/menu_mouton@2x.png',
+                    0xFFF98885, 'Sheep', 1),
+                GameRow('assets/images/Dashboard/menu_espace@2x.png',
+                    0xFF085559, 'Starship', 2),
+                GameRow('assets/images/Dashboard/menu_gobe@2x.png', 0xFF86A2A3,
+                    'Toad', 2),
+              ]
+              //wrap
+              ),
         ));
+  }
+}
+
+class GameRow extends StatelessWidget {
+  // const GameRow({Key? key}) : super(key: key);
+
+  GameRow(this.gameAsset, this.gameColorCode, this.title, this.numberOfSensors);
+
+  final String gameAsset;
+  int gameColorCode;
+  String title;
+  int numberOfSensors;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Color(gameColorCode))),
+        child: Container(
+            alignment: Alignment.centerLeft,
+            height: 100,
+            width: 400,
+            child: Row(//mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+              Image(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage(gameAsset)),
+              Spacer(),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.end,
+                      textScaleFactor: 2.0,
+                    ),
+                    SizedBox(height: 10),
+                    Image(
+                      alignment: Alignment.bottomRight,
+                      image: AssetImage('assets/images/Dashboard/capteur.png'),
+                      height: 25,
+                      width: 25,
+                    )
+                  ])
+            ])),
+        onPressed: () => Get.to(() => SheepGame()));
   }
 }
