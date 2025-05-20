ae620c8102926c64954ea53d7e9ddd050262b8ee
Caerwent <caerwent@gmail.com>
2025-05-20 11:32:38 +0200

    feat(settings): fix muscle settings

--- baahbox/lib/services/settings/generalSettingsMusclePage.dart
+++ baahbox/lib/services/settings/generalSettingsMusclePage.dart
@@ -56,6 +56,88 @@ class MuscleSettingsView extends GetView<SettingsController> {
                   onChanged: (bool newValue) {
                     controller.setMuscle2To(newValue);
                   }))),
-        ]));
+              Card(
+                  shape: ContinuousRectangleBorder(),
+                  child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Sensibilité',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            const Text(
+                              'Paramétrez la sensibilité des capteurs',
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ]))),
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 8),
+                  child: const Text(
+                    'Sensibilité',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  )),
+              SensitivitySelectionView()
+
+            ]));
+  }
+
+
+}
+
+class SensitivitySelectionView extends StatefulWidget {
+  const SensitivitySelectionView({super.key});
+
+  @override
+  State<SensitivitySelectionView> createState() =>
+      _SensitivitySelectionViewState();
+}
+
+class _SensitivitySelectionViewState extends State<SensitivitySelectionView> {
+  final SettingsController controller = Get.find();
+  late Sensitivity? _selection;
+  void onSelectionChanged(Sensitivity? value) {
+    setState(() {
+      _selection = value;
+      if (value != null) {
+        controller.updateSensitivityTo(value);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _selection = Sensitivity.medium;
+    return Column(
+      children: <Widget>[
+        RadioListTile.adaptive(
+            title: const Text('Faible'),
+            value: Sensitivity.low,
+            groupValue: _selection,
+            toggleable: true,
+            onChanged: onSelectionChanged),
+        RadioListTile.adaptive(
+            title: const Text('Moyenne'),
+            value: Sensitivity.medium,
+            groupValue: _selection,
+            toggleable: true,
+            onChanged: onSelectionChanged),
+        RadioListTile.adaptive(
+            title: const Text('Elevée'),
+            value: Sensitivity.high,
+            groupValue: _selection,
+            toggleable: true,
+            onChanged: onSelectionChanged),
+      ],
+    );
   }
 }
